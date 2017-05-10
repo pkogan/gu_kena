08e570aa615dcb468ddfc00a8df5dec9e4670c80
celeste <celeste@equipoCele>
2017-05-09 21:58:25 -0300

    Elecciones 2017 Estudiantes Mayo

--- metadatos/tablas/apex_fuente_datos.sql
+++ metadatos/tablas/apex_fuente_datos.sql
@@ -17,7 +17,7 @@ INSERT INTO apex_fuente_datos (proyecto, fuente_datos, descripcion, descripcion_
 	'gu_kena', --instancia_id
 	NULL, --administrador
 	'1', --link_instancia
-	'0', --tiene_auditoria
+	'1', --tiene_auditoria
 	'0', --parsea_errores
 	'0', --permisos_por_tabla
 	NULL, --usuario
